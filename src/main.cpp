65f6e6d8a89e04575ce29b111f5ddbe05a8a1cbd
norad32 <acc@fredyhirt.ch>
2026-07-01 12:38:04 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,20 @@
 #include <Animation.h>
 #include <Arduino.h>
 #include <Bgr.h>
-#include <Complex.h>
-#include <KnightRider.h>
 #include <LedColor.h>
 #include <LedFrame.h>
 #include <QwiicLedStick.h>
-#include <Rainbow.h>
+#include <Sparkfun_Qwiic_Button.h>
 #include <config/board.h>
 
 static LED leds = LED();
+static QwiicButton button = QwiicButton();
 
-static unsigned int currentAnimationIndex = 0;
-static unsigned long currentAnimationStartMs = 0;
-static unsigned long currentFrameStartMs = 0;
-static unsigned long animationFrameCounter = 0;
+static Frame frame;
 
-static Frame currentFrame;
+static const Animation animation = {"B-G-R sequence", renderBgr, 6000};
 
-static const Animation animations[] = {
-    {"B-G-R sequence", renderBgr, 6000},
-    {"Knight Rider", renderKnightRider, 8000},
-    {"Complex animation", renderComplexAnimation, 6000},
-    {"Complex + LED0 R-G-B-Off", renderComplexAnimationWithLed0RgbOff, 8000},
-    {"Complex + LED0 Rainbow", renderComplexAnimationWithLed0Rainbow, 8000},
-    {"Static Rainbow", renderStaticRainbow, 5000},
-    {"Running Rainbow", renderRunningRainbow, 10000},
-};
-
-static const unsigned int animationCount = sizeof(animations) / sizeof(animations[0]);
+static void button_wait_for_press(QwiicButton *const button);
 
 void setup(void)
 {
@@ -44,6 +30,11 @@ void setup(void)
     leds.setLEDBrightness(1);
     Serial.println(" done");
 
+    Serial.println("Initializing Button...");
+    success = success && button.begin();
+    button.LEDconfig(1, 0, 0);
+    Serial.println(" done");
+
     if (!success)
     {
         Serial.println("Initialization failed at some point. Halting...");
@@ -55,48 +46,36 @@ void setup(void)
     {
         Serial.println("Starting");
     }
-
-    currentAnimationStartMs = millis();
-    currentFrameStartMs = millis();
-
-    animations[currentAnimationIndex].render(&currentFrame, animationFrameCounter);
-    showFrame(&currentFrame, &leds);
 }
 
 void loop(void)
 {
     // put your main code here, to run repeatedly:
-    unsigned long now = millis();
+    animation.render(&frame, 0);
+    showFrame(&frame, &leds);
+    delay(frame.duration_ms);
 
-    Animation currentAnimation = animations[currentAnimationIndex];
+    animation.render(&frame, 1);
+    showFrame(&frame, &leds);
+    delay(frame.duration_ms);
 
-    if (now - currentAnimationStartMs >= currentAnimation.duration_ms)
-    {
-        currentAnimationIndex++;
+    animation.render(&frame, 2);
+    showFrame(&frame, &leds);
+    delay(frame.duration_ms);
 
-        if (currentAnimationIndex >= animationCount)
-        {
-            currentAnimationIndex = 0;
-        }
-
-        currentAnimationStartMs = now;
-        currentFrameStartMs = now;
-        animationFrameCounter = 0;
-
-        Serial.print("Switching to animation: ");
-        Serial.println(animations[currentAnimationIndex].name);
-
-        animations[currentAnimationIndex].render(&currentFrame, animationFrameCounter);
-        showFrame(&currentFrame, &leds);
-        return;
-    }
-
-    if (now - currentFrameStartMs >= currentFrame.duration_ms)
-    {
-        currentFrameStartMs = now;
-        animationFrameCounter++;
-
-        animations[currentAnimationIndex].render(&currentFrame, animationFrameCounter);
-        showFrame(&currentFrame, &leds);
-    }
+    button_wait_for_press(&button);
 }
+
+static void button_wait_for_press(QwiicButton *const button)
+{
+    button->clearEventBits();
+
+    while (!button->hasBeenClicked())
+    {
+        delay(20);
+    }
+
+    button->clearEventBits();
+    Serial.println("Button pressed");
+}
+
